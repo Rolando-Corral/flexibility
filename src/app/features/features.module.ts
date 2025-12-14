b44f47ac7068a3571492abb32943b43000228a85
Rolando <rolando.corral.js@gmail.com>
2025-12-13 22:09:01 -0300

    titulo flexible configurable - muevo el componente

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -5,6 +5,7 @@ import { FeaturesRoutingModule } from './features-routing.module';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { DesingSystemShowroomComponent } from './desing-system-showroom/desing-system-showroom.component';
 import { GenericButtonComponent } from '../shared/atoms/generic-button/generic-button.component';
+import { GenericTitleComponent } from '../shared/molecules/generic-title/generic-title.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { GenericButtonComponent } from '../shared/atoms/generic-button/generic-b
   imports: [
     CommonModule,
     FeaturesRoutingModule,
-    GenericButtonComponent
+    GenericButtonComponent,
+    GenericTitleComponent
   ]
 })
 export class FeaturesModule { }
